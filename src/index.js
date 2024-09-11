572f8436f9ba9126141b6afddb8348bac6d6d004
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-11 22:17:29 +0300

    solve a bug

--- src/index.js
+++ src/index.js
@@ -1,81 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./index.css";
-
-const App = () => {
+function App() {
   return (
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
+    <div>
+      <h1>Hello World</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
     </div>
   );
-};
-
-function Header() {
-  return (
-    <header className="header">
-      <h1>Fast React Pizze Co.</h1>;
-    </header>
-  );
 }
 
-function Menu() {
+function Pizza() {
   return (
-    <main className="menu">
-      <h2>Our Menu</h2>
-      <Pizza
-        name="Focaccia"
-        ingredients="Bread with italian olive oil and rosemary"
-        photoName="pizzas/focaccia.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Margherita"
-        ingredients="Tomato and mozarella"
-        photoName="pizzas/margherita.jpg"
-        price={10}
-      />
-      <Pizza
-        name="Pizza Spinaci"
-        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-        photoName="pizzas/spinaci.jpg"
-        price={10}
-      />
-    </main>
-  );
-}
-
-function Footer() {
-  const hour = new Date().getHours();
-  const openHour = 12;
-  const closeHour = 22;
-  const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-  // if (hour >= openHour && hour <= closeHour) alert("We are currently Open!");
-  // else alert("Sorry we're closed");
-
-  return (
-    <footer className="footer">
-      {new Date().toLocaleTimeString()}. We are currently open
-    </footer>
-  );
-}
-
-function Pizza(props) {
-  return (
-    <div className="pizza">
-      <img src={props.photoName} alt="Pizza facaccia" />
-      <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{+props.price}</span>
-      </div>
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta chesse</p>
     </div>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
