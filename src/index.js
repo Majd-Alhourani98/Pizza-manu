9043c675689de892e9bdfcb186dddaf14bcc7b39
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2024-09-13 18:30:40 +0300

    rendering pizzas conditioally

--- src/index.js
+++ src/index.js
@@ -67,19 +67,26 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = [pizzaData];
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {pizzas.length > 0 ? (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
+      )}
     </main>
   );
 }
 
 function Pizza(props) {
+  if (props.pizzaObj.soldOut) return null;
+
   return (
     <li className="pizza">
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
@@ -92,9 +99,20 @@ function Pizza(props) {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 20;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open
+      {isOpen ? (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us to order online.</p>
+          <button className="btn">Order</button>{" "}
+        </div>
+      ) : null}
     </footer>
   );
 }
